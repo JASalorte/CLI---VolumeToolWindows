641ecc00d959e0d77c52ae618efd9bbc1b43df8d
LinkkuLegend <salazarcontactinfo@gmail.com>
2025-10-10 11:01:01 +0200

    Removed match/case for 3.8

--- audio_tool/cli.py
+++ audio_tool/cli.py
@@ -33,50 +33,49 @@ def main():
 
     args = parser.parse_args()
 
-    match args.command:
-        case "list":
+    if args.command == "list":
             for session, _ in list_sessions_verbose():
                 print(session)
-        case "select":
-            sessions = list_sessions_verbose(list_pos=True)
-            if not sessions:
-                print("No audio sessions found.")
-                pass
+    elif args.command == "select":
+        sessions = list_sessions_verbose(list_pos=True)
+        if not sessions:
+            print("No audio sessions found.")
+            pass
 
-            sessions_print, sessions_raw = zip(*sessions)
-            for session_formatted in sessions_print:
-                print(session_formatted)
+        sessions_print, sessions_raw = zip(*sessions)
+        for session_formatted in sessions_print:
+            print(session_formatted)
 
-            results = _interactive_set_volume(sessions_raw)
-            for r in results:
-                if r.error:
-                    print(r.error.value)
-                else:
-                    print(f"Volume of {r.name} set to {r.volume * 100:.0f}%")
-        case "set":
-            results = set_volume_by_name(args.app_name, args.volume)
-            for r in results:
-                if r.error:
-                    print(r.error.value)
-                else:
-                    print(f"Volume of {r.name} set to {r.volume * 100:.0f}%")
-        case "toggle":
-            results = toggle_volume(args.app_name)
-            for r in results:
-                if r.error:
-                    print(r.error.value)
-                else:
-                    mute_status = "muted" if r.muted else "unmuted"
-                    print(f"{r.name} is now {mute_status}.")
-        case "cdda":
-            # Special case for Cataclysm DDA
-            new_vol = toggle_volume("cataclysm-tiles.exe")
-            for r in new_vol:
-                if r.error:
-                    print(r.error.value)
-                else:
-                    mute_status = "muted" if r.muted else "unmuted"
-                    print(f"CDDA is now {mute_status}.")
+        results = _interactive_set_volume(sessions_raw)
+        for r in results:
+            if r.error:
+                print(r.error.value)
+            else:
+                print(f"Volume of {r.name} set to {r.volume * 100:.0f}%")
+    elif args.command == "set":
+        results = set_volume_by_name(args.app_name, args.volume)
+        for r in results:
+            if r.error:
+                print(r.error.value)
+            else:
+                print(f"Volume of {r.name} set to {r.volume * 100:.0f}%")
+    elif args.command == "toggle":
+        results = toggle_volume(args.app_name)
+        for r in results:
+            if r.error:
+                print(r.error.value)
+            else:
+                mute_status = "muted" if r.muted else "unmuted"
+                print(f"{r.name} is now {mute_status}.")
+    elif args.command == "cdda":
+        # Special case for Cataclysm DDA
+        new_vol = toggle_volume("cataclysm-tiles.exe")
+        for r in new_vol:
+            if r.error:
+                print(r.error.value)
+            else:
+                mute_status = "muted" if r.muted else "unmuted"
+                print(f"CDDA is now {mute_status}.")
 
 
 
